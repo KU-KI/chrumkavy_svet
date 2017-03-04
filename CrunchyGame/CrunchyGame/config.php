bed386ff2778fd31d6dc2e2cbeb070659e1adcc7
patrikporuban <porubanpatrik@gmail.com>
2017-03-04 11:50:03 +0100

    new connect file

--- CrunchyGame/CrunchyGame/config.php
+++ CrunchyGame/CrunchyGame/config.php
@@ -1,5 +1,5 @@
 <?php
-try {
+/*try {
     $conn = new PDO("sqlsrv:server = tcp:crunchygamedatabase.database.windows.net,1433; Database = crunchygame", "crunchygame", "Gameoftheyear2017");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
@@ -9,5 +9,16 @@ catch (PDOException $e) {
 }
 if($conn == true){
     print("Pripojenie nadviazane</br>");
+}*/
+$servername = "tcp:crunchygamedatabase.database.windows.net,1433";
+$username = "crunchygame";
+$password = "Gameoftheyear2017";
+$dbname = "crunchygame";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
