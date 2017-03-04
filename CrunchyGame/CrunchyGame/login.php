e8b6f4dd95d4eda697d1a7862371a5e4cd1513d2
patrikporuban <porubanpatrik@gmail.com>
2017-03-04 12:09:27 +0100

    X

--- CrunchyGame/CrunchyGame/login.php
+++ CrunchyGame/CrunchyGame/login.php
@@ -46,18 +46,22 @@
         }
         $conn->close();*/
 
-        // sql to create table
-        $sql = "CREATE TABLE MyGuests (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        firstname VARCHAR(30) NOT NULL,
-        lastname VARCHAR(30) NOT NULL,
-        email VARCHAR(50),
-        reg_date TIMESTAMP
-        )";
+        try {
+            $sql = "CREATE TABLE MyGuests (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
+            reg_date TIMESTAMP
+            )";
+            $conn->exec($sql);
+            echo "Table MyGuests created successfully";
+            }
+        catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
 
-        // use exec() because no results are returned
-        $conn->exec($sql);
-        echo "Table MyGuests created successfully";
         $conn = null;
     ?>
 </body>
