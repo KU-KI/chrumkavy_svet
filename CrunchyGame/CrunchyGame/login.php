9d42918f553218bd00ab99c5d9747f80eb586be1
patrikporuban <porubanpatrik@gmail.com>
2017-03-04 11:18:18 +0100

    login

--- CrunchyGame/CrunchyGame/login.php
+++ CrunchyGame/CrunchyGame/login.php
@@ -32,5 +32,9 @@
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
     <script src="assets/js/index.js"></script>
+    <?php
+        include 'config.php';
+        echo $conn;
+    ?>
 </body>
 </html>
