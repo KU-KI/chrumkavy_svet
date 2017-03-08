8d565d05ea5ead381271c2fe3e0e87f3c07baabb
patrikporuban <porubanpatrik@gmail.com>
2017-03-08 10:44:52 +0100

    UPDATE

--- CrunchyGame/CrunchyGame/index.php
+++ CrunchyGame/CrunchyGame/index.php
@@ -1,24 +1,224 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Crunchy Game</title>
-    <!-- BOOTSTRAP STYLES-->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONTAWESOME STYLES-->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- MORRIS CHART STYLES-->
-    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
-    <!-- CUSTOM STYLES-->
-    <link href="assets/css/custom.css" rel="stylesheet" />
-    <!-- GOOGLE FONTS-->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>All Template Needs</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/custom-styles.css" rel="stylesheet">
+    <link href="assets/css/image-effects.css" rel="stylesheet">
+
+    <link href="assets/css/normalize.css" rel="stylesheet">
+    <link href="assets/css/component.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/font-awesome-ie7.css">
+
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <script src="js/modernizr.custom.js"></script>
+
 </head>
+
 <body>
-    <?php
-        echo 'Chrumkavý Svet!';
-        phpinfo();
-    ?>
+    <div class="container page-styling">
+        <div class="header-wrapper">
+            <div class="site-name">
+                <h1>Chrumkavý Svet</h1>
+            </div>
+            <div class="menu">
+                <div class="navbar">
+
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav ">
+                            <li><a href="#">Domov</a></li>
+                            <li class="dropdown ">
+                                <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Test<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Another action</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="login.php">Prihlásenie</a></li>
+                        </ul>
+
+                    </div><!--/.navbar-collapse -->
+
+                </div>
+                <div class="mini-menu">
+                    <label>
+                        <select class="selectnav">
+                            <option value="#" selected="">Home</option>
+                            <option value="#">About</option>
+                            <option value="#">→ Another action</option>
+                            <option value="#">→ Something else here</option>
+                            <option value="#">→ Another action</option>
+                            <option value="#">→ Something else here</option>
+                            <option value="#">Services</option>
+                            <option value="#">Portfolio</option>
+                            <option value="#">Contact</option>
+                        </select>
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="banner">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="assets/img/banner-img.jpg" alt="" class="">
+                        <div class="carousel-caption">
+                            <i class="fw-icon-pencil"></i>
+                            <h2>Corem ipsum</h2>
+                            <h1>sit amet vivamus</h1>
+                            <a href="#" class="btn"> more info</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="assets/img/banner-img.jpg" alt="">
+                        <div class="carousel-caption">
+                            <i class="fw-icon-pencil"></i>
+                            <h2>Corem ipsum</h2>
+                            <h1>sit amet vivamus</h1>
+                            <a href="#" class="btn"> more info</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="assets/img/banner-img.jpg" alt="">
+                        <div class="carousel-caption">
+                            <i class="fw-icon-pencil"></i>
+                            <h2>Corem ipsum</h2>
+                            <h1>sit amet vivamus</h1>
+                            <a href="#" class="btn"> more info</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
+            </div>
+        </div>
+        <div class="content-wrap">
+            <div class="main-title">
+                <ul class="grid effect-8" id="grid">
+                    <li>
+                        <h1>Lorem ipsum</h1>
+                        <h4>
+                            Donec nec justo eget felis facilisis fermentum.<br>
+                            Aliquam porttit or mauris sit amet orci. Aenean dignissim pellentesque felis.
+                        </h4>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        <div class="lines">
+            <div class="more-btn">
+                <a href="#" class="btn">Read More</a>
+            </div>
+        </div>
+        <div class="content-wrap">
+        </div>
+        <div class="ruler"></div>
+        <div class="content-wrap centering">
+            <div class="main-title spacing-bt">
+                <ul class="grid effect-8" id="grid">
+                    <li>
+                        <h1>Cras iaculis</h1>
+                        <h4>Fusce lobortis lorem at ipsum semper sagittis</h4>
+                    </li>
+                </ul>
+            </div>
+            <div class="featured-items">
+                <ul class="grid effect-2" id="grid">
+                    <li>
+                        <div class="item">
+                            <h5>Vestibulum commodo<br> felis quis tortor</h5>
+                            <div class="grow">
+                                <img src="assets/img/img1.jpg" alt="">
+                            </div>
+                            <p>Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.</p>
+                            <p><a href="#" class="more">more info <i class="fw-icon-chevron-right"></i></a></p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="item">
+                            <h5>Donec quis dui at<br> dolor</h5>
+                            <div class="grow">
+                                <img src="assets/img/img2.jpg" alt="">
+                            </div>
+                            <p>Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscing risus</p>
+                            <p><a href="#" class="more">more info <i class="fw-icon-chevron-right"></i></a></p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="item">
+                            <h5>Vestibulum commodo<br> felis quis tortor</h5>
+                            <div class="grow">
+                                <img src="assets/img/img3.jpg" alt="">
+                            </div>
+                            <p>Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.</p>
+                            <p><a href="#" class="more">more info <i class="fw-icon-chevron-right"></i></a></p>
+                        </div>
+                    </li>
+                </ul>
+
+
+
+            </div>
+        </div>
+
+    </div><!-- /container -->
+
+    <div class="container">
+        <div class="copy-rights">
+            Copyright(c) Chrumkavý Svet<br>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery-1.9.1.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <script src="assets/js/masonry.pkgd.min.js"></script>
+    <script src="assets/js/imagesloaded.js"></script>
+    <script src="assets/js/classie.js"></script>
+    <script src="assets/js/AnimOnScroll.js"></script>
+
+    <script>
+      new AnimOnScroll( document.getElementById( 'grid' ), {
+        minDuration : 0.4,
+        maxDuration : 0.7,
+        viewportFactor : 0.2
+      } );
+    </script>
+
+
 </body>
 </html>
+
