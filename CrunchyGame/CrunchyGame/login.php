fb186113dcd25037465ecc97985a6ff081937cf3
Patrik Porubän <porubanpatrik@hotmail.com>
2017-03-26 09:30:44 +0200

    FIX

--- CrunchyGame/CrunchyGame/login.php
+++ CrunchyGame/CrunchyGame/login.php
@@ -2,21 +2,13 @@
 include("config.php");
 session_start();
 
-if ($db->connect_error) {
-    echo "Not connected, error: " . $db->connect_error;
-}
-else
-{
-    echo "Connected.";
-}
-
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     // username and password sent from form
 
     $myusername = mysqli_real_escape_string($db,$_POST['username']);
     $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-    $sql = "SELECT id FROM account WHERE username = '$myusername' and passcode = '$mypassword'";
+    $sql = "SELECT id FROM account WHERE username = '$myusername' and password = '$mypassword'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
