90b080ed03fc2f4b16990ee3ccc11c7ba58b4dcf
patrikporuban <porubanpatrik@gmail.com>
2017-04-09 08:34:49 +0200

    test

--- CrunchyGame/CrunchyGame/settings.php
+++ CrunchyGame/CrunchyGame/settings.php
@@ -79,19 +79,19 @@ if (isset($_POST['zmenheslo']))
             <h1>Vyber si avatar</h1>
             <table style="width:100%">
                 <tr>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Age</th>
-                </tr>
-                <tr>
-                    <td>Jill</td>
-                    <td>Smith</td>
+                    <td><img src="default.png" alt="1" style="width:240px;height:240px;"></td>
+                    <td><img src="1.png" alt="1" style="width:240px;height:240px;"></td>
                     <td>50</td>
                 </tr>
                 <tr>
                     <td>Eve</td>
                     <td>Jackson</td>
                     <td>94</td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="1.png" alt="1" style="width:240px;height:240px;" />
+                    </td>
                 </tr>
             </table>
     </div>
